91f7f06c4c8a929e411628b7c8978913c92e7d46
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-08-24 16:21:12 -0700

    Update binaries

--- web/dist/main.js
+++ web/dist/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:173be17b5cf2789c6fea397719e1b0a3d15c74dae55944b3d21d489204d3971e
-size 173794
+oid sha256:ac33cb0c46857f629870475989b78b3fa8e8afa471e73490f78d673d49baaf82
+size 166230
