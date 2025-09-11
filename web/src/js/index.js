a4c04f30c1eb116b3d77be97e01da40f350939fe
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-09-11 12:20:01 -0700

    Bring back in FormattedOutput helper

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -1,21 +1,52 @@
 var Module = require("Formatter/out");
 require("bootstrap");
 
-function printErr(e) {
+async function load() {
 	let output = document.getElementById("output-text");
-	output.innerText += e + "\n";
-	output.classList.add("code-error");
-}
+	function printErr(e) {
+		output.innerText += e + "\n";
+		output.classList.add("code-error");
+	}
+	
+	let m = await Module({
+		printErr: printErr,
+		print: (p) => {
+			output.innerText += p + "\n";
+		}
+	});
 
-Module({
-	printErr: printErr
-}).then((m) => {
-	let output = document.getElementById("output-text");
-	output.classList.remove("code-error");
-	output.innerText = "_";
+	class Formatter {
+		static beginExternalFormattedOutput = m.cwrap("BeginExternalFormattedOutput", "number", ["string", "number"]);
+		static outputAscii = m.cwrap("OutputAscii", "number", ["number", "string", "number"])
+		static endIo = m.cwrap("EndIoStatement", "", ["number"]);
+	}
+
+	class FormattedOutput {
+		#io;
+		constructor(formatString) {
+			this.#io = Formatter.beginExternalFormattedOutput(formatString, formatString.length);
+		}
+
+		addAscii(string) {
+			Formatter.outputAscii(this.#io, string, string.length);
+		}
+
+		print() {
+			Formatter.endIo(this.#io);
+		}
+	}
+
+	output.innerText = "";
 	try {
-		console.log(m.ccall("test", "", [], []));
+		let f = new FormattedOutput("('Testing hellooo' I2)");
+		// TODO: Need to save the above string somewhere, otherwise this throws it off:
+		// f.addAscii("Test");
+		// f.outputAscii(io, );
+		f.print();
+		// m.ccall("_FortranAioOutputAscii", "number", ["string", "number"], ["('Hello')", 9]);
 	} catch (e) {
 		printErr(e);
 	}
-});
+}
+
+document.addEventListener("DOMContentLoaded", load);
